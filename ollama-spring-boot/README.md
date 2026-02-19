c22c49038528d6883d0eaa350c233071128b25ff
Vansh Sharma <sharmavansh7932@gmail.com>
2026-02-19 23:33:34 +0530

    ollama-spring-boot

---
+++ ollama-spring-boot/README.md
@@ -0,0 +1,10 @@
+Project Description  
+        This Spring Boot project demonstrates integration with Ollama to run Large Language Models locally. It enables AI-powered responses without using external APIs, ensuring offline capability, data privacy, and cost-free local inference
+
+   Tech Stack
+    Java Version 17 
+    Spring Boot Version 4.0.2
+    Dependency 
+        : Spring Web
+        : Ollama-model
+        : Lombok
---
+++ ollama-spring-boot/src/main/java/com/controller/OllamaController.java
@@ -0,0 +1,26 @@
+package com.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OllamaController {
+    ChatClient chatClient;
+
+    public OllamaController(ChatClient.Builder chatClientbuilder) {
+        this.chatClient = chatClientbuilder.build();
+    }
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam(value = "message" ,defaultValue = "what is the capital of India ?") String message) {
+        return chatClient.prompt()
+                .user(message)
+                .call()
+                .chatResponse().getResult().getOutput().getText();
+    }
+}
+
+// ollama --version
+
